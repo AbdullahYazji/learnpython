890e677924a5158ec585d012bc0651c24e78fb12
AbdullahYazji <129000798+AbdullahYazji@users.noreply.github.com>
2023-06-13 22:32:18 +0300

    pyth

---
+++ pyth.py
@@ -0,0 +1,14 @@
+print("Hello world")
+print(1+2)
+text ="abdullah"
+age  ="18"
+print("My name is " + text)
+print("I'm " + age + " years old")
+print(text.lower())
+print(text.upper())
+print(text.islower())
+print(text.isupper())
+print(text.upper().isupper())
+print(text.islower())
+print(text.upper())
+print(text.isupper())
