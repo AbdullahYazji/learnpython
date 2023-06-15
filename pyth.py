4f2ba083f3bc6f35dcbc0c461afa7adde41fcd07
AbdullahYazji <129000798+AbdullahYazji@users.noreply.github.com>
2023-06-15 10:52:43 +0300

    pyth

--- pyth.py
+++ pyth.py
@@ -1,7 +1,7 @@
 print("Hello world")
 print(1+2)
-text ="abdullah"
-age  ="18"
+text = "abdullah"
+age = "18"
 print("My name is " + text)
 print("I'm " + age + " years old")
 print(text.lower())
@@ -12,3 +12,31 @@ print(text.upper().isupper())
 print(text.islower())
 print(text.upper())
 print(text.isupper())
+print(len(text))
+print(len("yazj"))
+print(text[0])
+print(text[5])
+last_name = "yazji"
+print(text[3], last_name[2])
+name = "Abdullah Yazji"
+print(name.index("l"))
+print(name.index("Yazji"))
+print(name.replace("Yazji", "yi"))
+
+print(1+5)
+num = 5
+print(num)
+print(str(num) + " my favorite number")
+my_num = -6
+print(my_num)
+print(abs(my_num))
+print(pow(2, 3))
+print(max(9, 5))
+print(min(9, 5))
+print(round(3.7))
+from math import *
+print(floor(3.5))
+print(ceil(3.5))
+print(sqrt(9))
+
+
