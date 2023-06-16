599887681631b9ce9c5b7eee634da14bf5268743
AbdullahYazji <129000798+AbdullahYazji@users.noreply.github.com>
2023-06-16 16:59:25 +0300

    Revert "Revert "py_file.py""
    This reverts commit c5f45a8cc31667265cff5e29e4d341a42b4fcc59.

---
+++ py_file.py
@@ -0,0 +1,9 @@
+languages = ["c_sharp", "html", "python", "java"]
+print(languages)
+print(languages[0])
+print(languages[0:2])
+print(languages[2:4])
+print(languages[0:])
+languages[1] = "css"
+print(languages[1])
+print(languages[0:4])
