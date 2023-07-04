d2fe87150edeeb241a964ec3faf8032fd18b7749
AbdullahYazji <129000798+AbdullahYazji@users.noreply.github.com>
2023-07-04 09:38:29 +0300

    py_file.py

--- py_file.py
+++ py_file.py
@@ -1,9 +1,29 @@
+# languages = ["c_sharp", "html", "python", "java"]
+# print(languages)
+# print(languages[0])
+# print(languages[0:2])
+# print(languages[2:4])
+# print(languages[0:])
+# languages[1] = "css"
+# print(languages[1])
+# print(languages[0:4])
+# _______________________________________________________________
 languages = ["c_sharp", "html", "python", "java"]
+apk = ["pycharm", "VSCode", "GitHub"]
+print(languages, apk)
+print(languages.index("html"))
+# languages.extend(apk)
+# print(languages)
+languages += apk
 print(languages)
-print(languages[0])
-print(languages[0:2])
-print(languages[2:4])
-print(languages[0:])
-languages[1] = "css"
-print(languages[1])
-print(languages[0:4])
+languages.append("css")
+print(languages)
+languages.insert(0, "javascript")
+print(languages)
+languages.remove("html")
+print(languages)
+languages.clear()
+print(languages)
+numbers = [33, 495, 56, 928]
+numbers.pop()
+print(numbers)
